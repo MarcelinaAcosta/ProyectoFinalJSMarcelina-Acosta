042d8532e65e98d7e5fb106a8e9ed83ad355e0bc
Marcelina Acosta <marcelina.acosta.1998@gmail.com>
2023-07-24 09:54:02 -0300

    en esta tercera entrega, realice los cambios sugeridos y agregue nuevas funcionalidades, como almacenar y traer del local storage, eliminar, agregar tarjetas, iterar el array y mostrar todo en pantalla

--- js/index.js
+++ js/index.js
@@ -1,56 +1,109 @@
+let buttonPlusCard = document.getElementById('buttonPlusCard');
+let buttonOcultarMostrarListado = document.getElementById('OcultarMostrarListado');
+// let eliminarFlashCard = document.getElementById('eliminarFlashcard');
+
+let TarjetaListadoParse = JSON.parse(localStorage.getItem('listadosDeCards'));
+
+
+
 let TarjetasListado = []
 
 function Tarjeta(verbo, past, participle) {
     this.verbo = verbo;
     this.past = past;
     this.participle = participle
-}
+} // funcion constructora de los objetos.
 
-const tarjeta1 = TarjetasListado.push(new Tarjeta("be - Ser o estar", "was/were" , "been"));
-const tarjeta2 = TarjetasListado.push(new Tarjeta("become - Llegar a ser", "became" ,"become"));
-const tarjeta3 = TarjetasListado.push(new Tarjeta("begin - comenzar", "began" , "begun"));
-const tarjeta4 = TarjetasListado.push(new Tarjeta("break - Romper", "broke" , "broken"));
-const tarjeta5 = TarjetasListado.push(new Tarjeta("bring - traer", "brought" , "brought"));
-const tarjeta6 = TarjetasListado.push(new Tarjeta("build - construir", "built" , "built"));
-const tarjeta7 = TarjetasListado.push(new Tarjeta("buy - comprar", "bought" , "bought"));
-const tarjeta8 = TarjetasListado.push(new Tarjeta("can - poder", "could" , "---"));
-const tarjeta9= TarjetasListado.push(new Tarjeta("catch - agarrar", "caught" , "caught"));
-const tarjeta10 = TarjetasListado.push(new Tarjeta("choose - elegir", "was/were" , "been"));
-const tarjeta11 = TarjetasListado.push(new Tarjeta("come - venir", "came" , "come"));
-const tarjeta12 = TarjetasListado.push(new Tarjeta("cost - costar", "cost" , "cost"));
-const tarjeta13 = TarjetasListado.push(new Tarjeta("cut - cortar", "cut" , "cut"));
-const tarjeta14 = TarjetasListado.push(new Tarjeta("do - hacer", "did" , "done"));
-const tarjeta15 = TarjetasListado.push(new Tarjeta("dream - soñar", "dreamt (also dreamed)" , "dreamt (also dreamed)"));
-const tarjeta16 = TarjetasListado.push(new Tarjeta("drink - beber", "drank" , "drunk"));
-const tarjeta17 = TarjetasListado.push(new Tarjeta("drive - conducir", "drove" , "driven"));
-const tarjeta18 = TarjetasListado.push(new Tarjeta("eat - comer", "ate" , "eaten"));
-const tarjeta19 = TarjetasListado.push(new Tarjeta("fall - caer", "fell" , "fallen"));
-const tarjeta20 = TarjetasListado.push(new Tarjeta("feel - sentir", "found" , "found"));
+//pusheando los objetos al array "TarjetaListado"
+TarjetasListado.push(new Tarjeta("be - Ser o estar", "was/were" , "been"));
+TarjetasListado.push(new Tarjeta("become - Llegar a ser", "became" ,"become"));
+TarjetasListado.push(new Tarjeta("begin - comenzar", "began" , "begun"));
+TarjetasListado.push(new Tarjeta("break - Romper", "broke" , "broken"));
+TarjetasListado.push(new Tarjeta("bring - traer", "brought" , "brought"));
+TarjetasListado.push(new Tarjeta("build - construir", "built" , "built"));
+TarjetasListado.push(new Tarjeta("buy - comprar", "bought" , "bought"));
+TarjetasListado.push(new Tarjeta("can - poder", "could" , "---"));
+TarjetasListado.push(new Tarjeta("catch - agarrar", "caught" , "caught"));
+TarjetasListado.push(new Tarjeta("choose - elegir", "was/were" , "been"));
+TarjetasListado.push(new Tarjeta("come - venir", "came" , "come"));
+TarjetasListado.push(new Tarjeta("cost - costar", "cost" , "cost"));
+TarjetasListado.push(new Tarjeta("cut - cortar", "cut" , "cut"));
+TarjetasListado.push(new Tarjeta("do - hacer", "did" , "done"));
+TarjetasListado.push(new Tarjeta("dream - soñar", "dreamt (also dreamed)" , "dreamt (also dreamed)"));
+TarjetasListado.push(new Tarjeta("drink - beber", "drank" , "drunk"));
+TarjetasListado.push(new Tarjeta("drive - conducir", "drove" , "driven"));
+TarjetasListado.push(new Tarjeta("eat - comer", "ate" , "eaten"));
+TarjetasListado.push(new Tarjeta("fall - caer", "fell" , "fallen"));
+TarjetasListado.push(new Tarjeta("feel - sentir", "found" , "found"));
 
-function mostrarCards () {
-    let html = "";
-    TarjetasListado.forEach(function (card) {
-    html += 'verbo en infinitivo: '+ card.verbo + '<br>' + 'Past simple: ' + card.past + '<br>' + 'presente perfecto: ' + card.participle + '<br>'
-    });
-    document.getElementById("card").innerHTML = html;
-}
+const CardsJSON = JSON.stringify(TarjetasListado);
+localStorage.setItem('ListadosDeCards', CardsJSON);
 
-mostrarCards();
-
-let buttonPlusCard = document.getElementById('buttonPlusCard');
-
-buttonPlusCard.addEventListener('click', () => {
+buttonPlusCard.addEventListener('click', () => { // aca estoy creando un evento para agregar datos a objetos nuevos
     let verboPrompt = prompt('Agregar verbo en infinitivo (con su traduccion entre paréntesis)');
     let pastPromt = prompt('Agregar verbo en pasado simple:');
     let participlePrompt = prompt('Agregar verbo en presente perfecto: ');
 
-    guardarDatosEnArray(verboPrompt, pastPromt, participlePrompt);
+    guardarDatosEnArray(verboPrompt, pastPromt, participlePrompt); // retorno los valores almacenados hacia la funcion "guardarDatosEnArray"
+    actualizarListadoEnPantalla();
 });
 
+function eliminarCard(index) { // elimina las cards del array en el local storage
+    TarjetaListadoParse.splice(index, 1);
+    localStorage.setItem('listadosDeCards', JSON.stringify(TarjetaListadoParse));
+    actualizarListadoEnPantalla();
+}
 
-function guardarDatosEnArray(dato1, dato2, dato3) {
 
+function guardarDatosEnArray(dato1, dato2, dato3) { // funcion que recibe los valores retornados en el evento y los pushea como objeto hacia el array
     TarjetasListado.push(new Tarjeta( dato1, dato2, dato3))
-    mostrarCards();
+    const CardsJSONactualizadas = JSON.stringify(TarjetasListado);
+    localStorage.setItem("listadosDeCards", CardsJSONactualizadas);
+    TarjetaListadoParse = JSON.parse(CardsJSONactualizadas); // Actualiza la variable TarjetaListadoParse
     
 };
+
+
+
+function actualizarListadoEnPantalla() { // funcion que imprime en el html la lista de flashCards
+        // 
+        let html = "";
+        TarjetaListadoParse.forEach(function (card, index) {
+            html += 'verbo en infinitivo: '+ card.verbo + '<br>' + 'Past simple: ' + card.past + '<br>' + 'presente perfecto: ' + card.participle + '<br>' + `<button onclick="eliminarCard(${index})">"Eliminar flashcard"</button><br>`;
+        });
+        document.getElementById("card").innerHTML = html;
+
+        
+    };
+
+    buttonOcultarMostrarListado.addEventListener('click', () => {
+        let ListadosDeArray = document.getElementById('card');
+        // Alterna la visibilidad del elemento con id "card"
+        ListadosDeArray.style.display = ListadosDeArray.style.display === 'none' ? 'block' : 'none';
+    });
+    
+    actualizarListadoEnPantalla();
+
+function rondaDeFlashCards(indice) { // funcion para mostrar objeto 1 por 1
+    let flashCardEnPantalla = TarjetasListado[indice];
+    let htmlFlashCard = 'verbo : ' + flashCardEnPantalla.verbo + 'past simple :' + flashCardEnPantalla.past + 'present perfect :' + flashCardEnPantalla.participle; 
+    // console.log(flashCardEnPantalla)
+    document.getElementById("rondaDeFlashCards").innerHTML = htmlFlashCard;
+    
+}
+let indiceActualFlashCard = 0;
+
+rondaDeFlashCards(indiceActualFlashCard);
+
+let btnSiguienteFlashCard = document.getElementById("mostrarSiguienteFlashCard");
+
+btnSiguienteFlashCard.addEventListener('click', () => { // evento creado para pasar de tarjeta a tarjeta mediante un boton 
+    indiceActualFlashCard++
+    if(indiceActualFlashCard < TarjetasListado.length){
+        rondaDeFlashCards(indiceActualFlashCard);
+    }
+    else{
+        htmlFlashCard2 += ' llegaste al final de la ronda !'
+        document.getElementById("rondaDeFlashCards").innerHTML = htmlFlashCard2;
+    }
+});
